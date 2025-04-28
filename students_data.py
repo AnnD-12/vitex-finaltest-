2eccfa218cb0fb99e34ea2c7213539aba5af11f8
AnnD-12 <stu715701078@hnue.edu.vn>
2025-04-29 00:01:10 +0700

    Update students_data.py

--- students_data.py
+++ students_data.py
@@ -19,6 +19,38 @@ students_data = {
 - Điểm cần cải thiện: Phát âm cần luyện tập thêm ở một số từ như image/ error/ storage/... và nên có lên xuống giọng, nhấn nhá để bài nói nghe tự tin hơn.""",
         "Feedback_Overall": """Em đánh giá chị Huế là điển hình cho câu nói “Có công mài sắt, có ngày nên kim”. Rất chăm học, chịu khó nên thực sự có thấy được sự tiến bộ rõ rệt. Không chỉ ở khoản nghe mà cả việc phát âm, em thấy đã đỡ tiếng địa phương đi nhiều rồi đó ah. Em cũng thấy sự tự tin của chị Huế khi nói nên rất tuyệt vời ạ. Gút chóp!"""
     },
-    ...
-    # Tiếp tục các học sinh khác
+    "Anh Sức": {
+        "Listening": 6,
+        "Vocabulary": 10,
+        "Phonetics": 10,
+        "Speaking": 8,
+        "Feedback_Speaking": """- Bài nói đáp ứng được yêu cầu của đề bài, có introduction, có role-play diễn ra rất tự nhiên và nhập tâm, có nêu ngữ cảnh tình huống rõ ràng.
+- Bài có sử dụng được những cấu trúc câu hỏi, có những từ “ah, uh, oh” được sử dụng đúng lúc đúng chỗ, giúp cho đoạn hội thoại nghe rất tự nhiên.
+- Từ vựng: sử dụng được những từ liên quan đến chủ đề và những từ đã học. Rất tốt ạ!
+- Điểm cần cải thiện: Anh Sức nên dành thêm thời gian luyện tập phát âm một số từ như “error, client, …” và các âm “s” nha ah. Đôi khi bị ngắc ngứ và dừng hơi lâu nên ảnh hưởng tới độ trôi chảy.""",
+        "Feedback_Overall": """Em đánh giá cao sự tiến bộ và tích cực của anh Sức trong những buổi học gần đây. Anh tiến bộ hơn nhiều trong khoản nghe, nói và cả từ vựng nắm được khá chắc. Rất đáng tuyên dương ạ! Hy vọng anh Sức có thể nhìn vào kết quả lần này để có thêm động lực “yêu thích” môn tiếng Anh và dành thêm một chút thời gian hàng ngày giải trí với bộ môn này sau những giờ căng thẳng chẳng hạn ah. :))))"""
+    },
+    "Quỳnh": {
+        "Listening": 7,
+        "Vocabulary": 10,
+        "Phonetics": 9,
+        "Speaking": 8,
+        "Feedback_Speaking": """- Bài nói đáp ứng được yêu cầu của đề bài, có introduction, có role-play rất tốt, có các cấu trúc cảm thán, câu hỏi rất đúng ngữ cảnh và tự nhiên.
+- Bài nêu được solution rõ ràng, sử dụng thêm các từ nối khiến bài có liên kết và mạch lạc hơn.
+- Từ vựng: Sử dụng được những từ liên quan đến chủ đề và các từ đã học, rất oke nuôn!
+- Điểm cần cải thiện: Phát âm cần luyện tập thêm, một số từ như “message/ customer/ …” Đôi lúc còn hơi ngập ngừng và dừng lâu để nghĩ từ, ảnh hưởng tới độ trôi chảy.""",
+        "Feedback_Overall": """Qua một thời gian t có thấy sự tiến bộ của m nha, đặc biệt trong khoản speaking. Mấy buổi gần đây phát âm có tiến bộ, nói cũng trôi chảy hơn và có cố gắng sử dụng các từ mới. Rất đáng tuyên dương! Hy vọng là m có thể dành thêm thời gian học từ, luyện nói ở nhà trước gương để tránh ngập ngừng nè. Không cần dùng những từ cao siêu đâu, mấu chốt của speaking là giao tiếp thành công - là nói mà người khác hiểu là được. Vậy nên cố gắng đừng overthinking lúc nói nha, đơn giản hóa thui. Gút chóp!"""
+    },
+    "Tuấn": {
+        "Listening": 8,
+        "Vocabulary": 10,
+        "Phonetics": 9,
+        "Speaking": 8.5,
+        "Feedback_Speaking": """- Bài nói đáp ứng được các yêu cầu của đề bài, có introduction, đưa ra problems, solutions và plan tương ứng.
+- Các thông tin được support kĩ và rõ ràng, sử dụng được các từ nối và signposts giúp các ý được rõ ràng và mạch lạc.
+- Bài nói có sử dụng được các cấu trúc so sánh, ngữ pháp khác nhau, nhìn chung là rất tốt đó ạ.
+- Từ vựng: Sử dụng được các từ vựng liên quan tới chủ đề và các từ đã học. Rất tốt ạ!
+- Điểm cần cải thiện: Anh Tuấn nên dành thêm thời gian luyện tập phát âm các từ như “message” hay các âm “s” là thiếu tương đối. Đôi lúc cũng bị ngập ngừng và ậm ừ nhiều, hơn nữa chưa có nhấn nhá, lên xuống giọng nên ảnh hưởng tới độ trôi chảy.""",
+        "Feedback_Overall": """Dù anh Tuấn học với em không lâu nhưng mà em thấy được anh Tuấn trình độ đỉnh ấy chứ. Không biết anh có ngồi nhầm lớp không :)))) Nhưng dù sao thì em có thấy được anh Tuấn nói rõ ràng hơn, các ý được trình bày ngắn gọn và đúng trọng tâm hơn, không bị dài dòng và khó hiểu như hồi trước. Hơi buồn vì phải bye bye anh nhưng em hy vọng thời gian vừa rồi thật sự anh có học được điều gì đó mới ở lớp và mong nó sẽ có ích cho anh trong tương lai. Chúc anh thành công trong công việc và đừng quên lớp tiếng Anh này nha ah! Hope to see you soon and wish you all the best!"""
+    }
 }
